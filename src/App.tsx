45c509dcf4903ff51217a989693ac23dafd3c468
Otávia Bedeschi <bedeschi.otavia@gmail.com>
2024-08-09 15:23:07 +0100

    Routes layout and Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
+import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
